b749b2a116d67bded5df8e3344a680918809501e
Sara Stavaski <sara.stavaski@slalom.com>
2021-12-08 11:03:10 -0800

    add service and deployment

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
 # This tags and uploads an image to Docker Hub
-# Step 1:
-# This is your Docker ID/path
+# Docker ID/path
 dockerpath=sarastavaski/capstone-containter
 
-# Step 2
 # Run the Docker Hub container with kubernetes
 kubectl run capstone-containter --image=sarastavaski/capstone-containter
 
-# Step 3:
 # List kubernetes pods
 kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
 kubectl port-forward capstone-containter 8000:80
 
-# Create the deployment
+# Create deployment
 kubectl apply -f deployment.yml
 
-# Create the service that is going to make your pods accessible
+# Create the service to make pods accessible
 kubectl apply -f service.yml
