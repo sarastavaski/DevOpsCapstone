52a8f23fcb56fe47be01ad2734ad914436b3ffef
Sara Stavaski <sara.stavaski@slalom.com>
2021-12-10 11:03:03 -0800

    config.yml update

--- update_docker.sh
+++ update_docker.sh
@@ -23,12 +23,14 @@ dockerpath=sarastavaski/capstone-containter
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-#docker login --username sarastavaski
-docker login --username $1 --password $2
+docker login --username sarastavaski
+#docker login --username $1 --password $2
 docker tag capstone-containter:latest $dockerpath:v2
 
 # Push image to docker repository
 docker push sarastavaski/capstone-containter:v2
+
+aws eks --region us-west-1 update-kubeconfig --name capstone-deployment
 
 # trigger rolling update
 kubectl set image deployment/capstone-rolling-update capstone-html-container=sarastavaski/capstone-containter:v2
